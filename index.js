c8ab58fe2beba338d29879b19e725234dbce7c16
mr.one.id <101521147+onepunya@users.noreply.github.com>
2024-04-16 23:33:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,28 +9,49 @@ const port = 3000;
 const CSS_URL =
   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 
-const swaggerOptions = {
-    definition: {
-    openapi: '3.0.0',
+const app = express();
+
+app.use(bodyParser.json()); // to use body object in requests
+const PORT = process.env.PORT || 2001;
+dotenv.config();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
     info: {
-      title: 'Your API Title',
-      version: '1.0.0',
-      description: 'API Documentation for Your API',
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+      termsOfService: "http://example.com/terms/",
+      contact: {
+        name: "API Support",
+        url: "http://www.exmaple.com/support",
+        email: "support@example.com",
+      },
     },
     servers: [
       {
-        url: 'http://localhost:3000',
+        url: "https://nodejs-swagger-api.vercel.app/",
+        description: "My API Documentation",
       },
     ],
   },
-  apis: ['./routes/*.js'],
+  // This is to call all the file
+  apis: ["'./routes/*.js'"],
 };
 
+const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-const swaggerSpec = swaggerJSDoc(swaggerOptions);
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
 
-
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { customCssUrl: CSS_URL }));
 app.use(routes);
 
 app.listen(port, () => {
